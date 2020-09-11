eb4ef6b50a4a04e9a14a41b9ec6c049ae1decc4a
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-09-10 23:29:10 -0300

    Completa 3. Agregando Emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5rem;
+`;
+
+const Boton = styled.button`
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2rem;
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  color: #ffffff;
+  border: 2px solid black;
+`;
 
 function App() {
-  return <h1>Breaking Bad</h1>;
+  const consultarApi = () => {
+    console.log("consultado...");
+  };
+  return (
+    <Contenedor>
+      <Boton onClick={consultarApi}>Obtener Frase</Boton>
+    </Contenedor>
+  );
 }
 
 export default App;
