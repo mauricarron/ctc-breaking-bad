f0e296864382d9ab867d721c4d75af348440de96
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-09-11 00:17:24 -0300

    Completa 7. CSS a la frase

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,12 +1,46 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const FraseBlockquote = styled.blockquote`
+  max-width: 800px;
+  padding: 3rem;
+  background-color: #ffffff;
+  border: 2px solid black;
+
+  @media (min-width: 768px) {
+    margin-top: 10rem;
+  }
+  p {
+    position: relative;
+    padding-left: 4rem;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 2rem;
+    text-align: center;
+    &::before {
+      content: open-quote;
+      font-size: 10rem;
+      color: black;
+      position: absolute;
+      top: -2rem;
+      left: -1rem;
+    }
+  }
+  footer {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1.4rem;
+    text-align: right;
+    color: #666666;
+    margin-top: 1rem;
+  }
+`;
 
 const Frase = ({ frase }) => {
   const { quote, author } = frase;
   return (
-    <blockquote>
+    <FraseBlockquote>
       <p>{quote}</p>
       <footer>— {author}</footer>
-    </blockquote>
+    </FraseBlockquote>
   );
 };
 
